76b99c95287f818bac56b36249911ec52795d03f
golap-hasan <golaphasan379@gmail.com>
2026-02-10 15:25:26 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
       {
         protocol: "https", 
         hostname: "images.unsplash.com", 
-      },
+      }, 
       { 
         protocol: "https",
         hostname: "res.cloudinary.com",
